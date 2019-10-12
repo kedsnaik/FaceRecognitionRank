0a7b38a4b64d3f47f7424edcf5c0e1f1a33ce7a3
kedsnaik <kedsnaik@gmail.com>
2019-10-13 00:06:46 +0530

    calculated the face box dimensions. Should style to make the box visible.

--- src/App.js
+++ src/App.js
@@ -40,7 +40,22 @@ const ParticlesOptions = {
   }
 
   calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    console.log(clarifaiFace);
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return{
+      leftCol : clarifaiFace.left_col * width,
+      topRow : clarifaiFace.top_row * height,
+      rightCol : width-clarifaiFace.right_col * width,
+      bottomRow : height-clarifaiFace.bottom_row * height
+    }
+  }
 
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box : box});
   }
 
   onButtonSubmit = (event) => {
@@ -50,14 +65,11 @@ const ParticlesOptions = {
     console.log('click');
     app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
     .then(
-      function(response) {
-        // console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-
-      },
-      function(err) {
-        // there was an error
-      }
-    );
+      (response) => {this.displayFaceBox(this.calculateFaceLocation(response));}
+    )
+    .catch(
+      (err => console.log(err))
+    )
   }
 
   render(){
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -4,7 +4,7 @@ const FaceRecognition = ({imageUrl}) => {
     return(
         <div className = 'center na'>
             <div className ='absolute mt2'>
-                <img alt = "face pic" src={imageUrl} width='500px' height='auto'/>
+                <img id = "inputimage" alt = "face pic" src={imageUrl} width='500px' height='auto'/>
             </div>
         </div>
     )
